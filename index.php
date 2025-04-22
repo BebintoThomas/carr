72d77076f079afd38a0587ed9395b56c32dc3dda
BebintoThomas <155514603+BebintoThomas@users.noreply.github.com>
2025-04-22 08:31:35 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+// Database credentials
+$servername = "my-mysql"; // This is the name of the MySQL container
+$username = "root"; 
+$password = "root"; 
+$dbname = "carren";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully to MySQL on Docker network!";
+?>
