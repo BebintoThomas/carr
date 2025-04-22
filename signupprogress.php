9dbdcb478d80ed3fad65bfda39c4937dcebd3b75
BebintoThomas <155514603+BebintoThomas@users.noreply.github.com>
2025-04-22 08:59:38 +0530

    Update signupprogress.php

--- signupprogress.php
+++ signupprogress.php
@@ -2,13 +2,13 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$servername = "localhost";
+$servername = "my-mysql";
 $username = "root";
 $password = "root"; // Adjust if your MySQL password is different
 $dbname = "carren";
-$port = 3307; // Adjust if your port is different (default is 3306)
+ // Adjust if your port is different (default is 3306)
 
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
