0a06cc7e7abd4e6480af2e3207aba4d3443133d7
BebintoThomas <155514603+BebintoThomas@users.noreply.github.com>
2025-04-22 08:58:53 +0530

    Update payment.php

--- payment.php
+++ payment.php
@@ -7,13 +7,13 @@ if (!isset($_SESSION['user_id'])) {
 }
 
 // Database connection to verify booking
-$servername = "localhost";
+$servername = "my-mysql";
 $username = "root";
 $password = "root";
 $dbname = "carren";
-$port = 3307;
 
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+$conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     error_log("Connection failed: " . $conn->connect_error);
     header("Location: dashbord.php?error=" . urlencode("Database connection error."));
