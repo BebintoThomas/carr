748a5543b8986c2ffee03c90c30db711ed95ab19
BebintoThomas <155514603+BebintoThomas@users.noreply.github.com>
2025-04-22 08:56:43 +0530

    Update paymentprogress.php

--- paymentprogress.php
+++ paymentprogress.php
@@ -11,13 +11,13 @@
     }
 
     // Database connection
-    $servername = "localhost";
+    $servername = "my-mysql";
     $username = "root";
     $password = "root";
     $dbname = "carren";
-    $port = 3307;
+    
 
-    $conn = new mysqli($servername, $username, $password, $dbname, $port);
+    $conn = new mysqli($servername, $username, $password, $dbname);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
